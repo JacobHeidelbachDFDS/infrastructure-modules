06b91eaaa5fd78fd82ddd7d5682bf1db96eb79ee
JacobHeidelbachDFDS <jfhei@dfds.com>
2020-03-28 22:34:37 +0100

    Added vars

--- database/postgres/vars.tf
+++ database/postgres/vars.tf
@@ -33,3 +33,18 @@ variable "skip_final_snapshot" {
   description = "Define if the default of creating a backup upon deletion should be skipped. Default is false"
   default = false
 }
+
+variable "db_instance_class" {
+  description = "The instance class of the DB (e.g. db.t2.micro)"
+  default     = "db.t2.micro"
+}
+
+variable "performance_insights_enabled" {
+  description = "Specifies whether Performance Insights are enabled. Default is false."
+  default = false
+}
+
+variable "performance_insights_retention_period" {
+  description = "The amount of time in days to retain Performance Insights data. Either 7 (7 days) or 731 (2 years). When specifying performance_insights_retention_period, performance_insights_enabled needs to be set to true. Default is '7'."
+  default = 7
+}
